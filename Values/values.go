58a7fa45a5886543537b702a0fbd563ced74921c
etherzeth <zethweb3@gmail.com>
2026-06-17 21:33:56 +0700

    three fold

---
+++ Values/values.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// string
+	fmt.Println("go" + "lang")
+
+	// int
+	fmt.Println("1+1 =", 1+1)
+	// float
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	// booleans
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
