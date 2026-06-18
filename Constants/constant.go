e089125f6bfdcb2349e88d4cc0e89b3b1851a8ab
etherzeth <zethweb3@gmail.com>
2026-06-18 13:53:41 +0700

    add: constant

---
+++ Constants/constant.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "constant"
+
+func main() {
+	fmt.Println(s)
+
+	const n = 500000000
+
+	const d = 3e20 / n
+	fmt.Println(d)
+
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(n))
+
+	// radian := n * math.Pi / 180
+	// fmt.Println(radian)
+	// fmt.Println(math.Sin(radian))
+}
