00d53cdb2c69ff1cf246e034335a3960ca81fe5f
etherzeth <zethweb3@gmail.com>
2026-06-21 18:56:07 +0700

    add: Closures

---
+++ Closures/closures.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+
+	nextInt := intSeq()
+
+	fmt.Println(nextInt()) // 1
+	fmt.Println(nextInt()) // 2
+	fmt.Println(nextInt()) // 3
+	fmt.Println(nextInt()) // 4
+
+	newInts := intSeq()
+	fmt.Println(newInts()) // 1
+}
